717a44d2b515f605421dbe2288bece2a60711a09
giuspek <giuseppespallitta@yahoo.it>
2020-05-04 00:12:21 +0200

    Removed -i option from main interface

--- main.cpp
+++ main.cpp
@@ -16,17 +16,12 @@ int main(int argc, char* argv[]){
   bool bv = false;
 	std::ofstream output;
 	std::ifstream input;
-	while ((c = getopt (argc, argv, "hi:o:b")) != -1)
+	while ((c = getopt (argc, argv, "ho:b")) != -1)
     switch (c)
       {
       case 'o':{
       	output.open(optarg);
         std::cout.rdbuf(output.rdbuf());
-        break;
-      }
-      case 'i':{
-      	input.open(optarg);
-        std::cin.rdbuf(input.rdbuf());
         break;
       }
       case 'b':{
@@ -36,7 +31,6 @@ int main(int argc, char* argv[]){
       case 'h':{
         std::cout<<"Usage: ./fzn2omt < INPUTFILE.fzn > OUTPUTFILE.smt2 [-b]"<<std::endl<<std::endl;
         std::cout<<"Alternatively you can pass files as command option arguments:"<<std::endl;
-        std::cout<<"-i INPUTFILE.fzn"<<std::endl;
         std::cout<<"-o OUTPUTFILE.smt2"<<std::endl<<std::endl;
         std::cout<<"-b can be used to represent Integers using BitVectors.";
         return 1;
